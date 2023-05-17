c7c4db3a278f836afd676b239e2be1d1d8104fa3
Jawad Afthab <E0969647@u.nus.edu>
2023-05-18 07:14:13 +0800

    changed home address

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <NavBar />
         <div className="container">
           <Routes>
-            <Route exact path="/" Component={Home}/>
+            <Route path="/home" Component={Home}/>
           </Routes>
 
           <Routes>
